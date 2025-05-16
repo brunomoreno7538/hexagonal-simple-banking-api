7ef1d8713dd1fa63a71e863fceee127bfafa3ce7
Bruno Moreno <brunomoreno7538@gmail.com>
2025-05-16 14:03:12 -0300

    Initial commit

---
+++ src/main/java/moreno/corebanking_natixis/CorebankingNatixisApplication.java
@@ -0,0 +1,13 @@
+package moreno.corebanking_natixis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorebankingNatixisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorebankingNatixisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/moreno/corebanking_natixis/CorebankingNatixisApplicationTests.java
@@ -0,0 +1,13 @@
+package moreno.corebanking_natixis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorebankingNatixisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
