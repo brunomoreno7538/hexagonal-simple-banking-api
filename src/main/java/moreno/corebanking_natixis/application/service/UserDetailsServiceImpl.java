651fe2aac280fa9f47c1d54ac2b13869c78f07a6
Bruno Moreno <brunomoreno7538@gmail.com>
2025-05-18 13:07:13 -0300

    Add merchant use in user details, to be able to authenticated himself

--- src/main/java/moreno/corebanking_natixis/application/service/UserDetailsServiceImpl.java
+++ src/main/java/moreno/corebanking_natixis/application/service/UserDetailsServiceImpl.java
@@ -2,7 +2,9 @@ package moreno.corebanking_natixis.application.service;
 
 import lombok.RequiredArgsConstructor;
 import moreno.corebanking_natixis.application.port.out.CoreUserRepository;
+import moreno.corebanking_natixis.application.port.out.MerchantUserRepository;
 import moreno.corebanking_natixis.domain.model.CoreUser;
+import moreno.corebanking_natixis.domain.model.MerchantUser;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -20,18 +23,39 @@ import java.util.Set;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final CoreUserRepository coreUserRepository;
+    private final MerchantUserRepository merchantUserRepository;
 
     @Override
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        CoreUser coreUser = coreUserRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        Optional<CoreUser> coreUserOptional = coreUserRepository.findByUsernameAndActiveTrue(username);
 
-        Set<GrantedAuthority> authorities = Collections.singleton(
-                new SimpleGrantedAuthority("ROLE_" + coreUser.getRole().name())
-        );
+        if (coreUserOptional.isPresent()) {
+            CoreUser coreUser = coreUserOptional.get();
+            if (!coreUser.isEnabled()) {
+                throw new UsernameNotFoundException("User " + username + " is disabled.");
+            }
+            Set<GrantedAuthority> authorities = Collections.singleton(
+                    new SimpleGrantedAuthority("ROLE_" + coreUser.getRole().name())
+            );
+            return new User(coreUser.getUsername(), coreUser.getPassword(), coreUser.isEnabled(),
+                    true, true, true, authorities);
+        }
 
-        return new User(coreUser.getUsername(), coreUser.getPassword(), coreUser.isEnabled(),
-                true, true, true, authorities);
+        Optional<MerchantUser> merchantUserOptional = merchantUserRepository.findByUsernameAndActiveTrue(username);
+
+        if (merchantUserOptional.isPresent()) {
+            MerchantUser merchantUser = merchantUserOptional.get();
+            if (!merchantUser.isEnabled()) {
+                throw new UsernameNotFoundException("User " + username + " is disabled.");
+            }
+            Set<GrantedAuthority> authorities = Collections.singleton(
+                    new SimpleGrantedAuthority("ROLE_" + merchantUser.getRole().name())
+            );
+            return new User(merchantUser.getUsername(), merchantUser.getPassword(), merchantUser.isEnabled(),
+                    true, true, true, authorities);
+        }
+
+        throw new UsernameNotFoundException("User not found or not active/enabled: " + username);
     }
 }
